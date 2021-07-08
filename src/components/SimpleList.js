3e2675cc970b747d7eba188ee29bd6ad5dd660c8
proelite <xiaodi.huang@alumni.duke.edu>
2021-07-08 00:21:08 -0700

    Added key key.

--- src/components/SimpleList.js
+++ src/components/SimpleList.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import PersonIcon from '@material-ui/icons/PersonOutline';
-import FileIcon from '@material-ui/icons/List';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import PersonIcon from "@material-ui/icons/PersonOutline";
+import FileIcon from "@material-ui/icons/List";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
+    width: "100%",
     maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
+    backgroundColor: theme.palette.background.paper
+  }
 }));
 
-
 function UserItem({ user, onUserItemClick }) {
-  console.log(user, onUserItemClick);
-
   return (
     <ListItem button onClick={() => onUserItemClick(user)}>
       <ListItemIcon>
         <PersonIcon />
       </ListItemIcon>
       <ListItemText primary={user} />
-    </ListItem>);
+    </ListItem>
+  );
 }
 
 function FileItem({ file, onFileItemClick }) {
@@ -36,24 +34,30 @@ function FileItem({ file, onFileItemClick }) {
         <FileIcon />
       </ListItemIcon>
       <ListItemText primary={file} />
-    </ListItem>);
+    </ListItem>
+  );
 }
 
-export default function SimpleList({ users, files, onUserItemClick, onFileItemClick}) {
+export default function SimpleList({
+  users,
+  files,
+  onUserItemClick,
+  onFileItemClick
+}) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <List component="nav" aria-label="Users">
-      {
-        users.map((user) => <UserItem user={user} onUserItemClick={onUserItemClick} />)
-      }
+        {users.map((user) => (
+          <UserItem key={user} user={user} onUserItemClick={onUserItemClick} />
+        ))}
       </List>
       <Divider />
       <List component="nav" aria-label="Files">
-      {
-        files.map((file) => <FileItem file={file} onFileItemClick={onFileItemClick} />)
-      }
+        {files.map((file) => (
+          <FileItem key={file} file={file} onFileItemClick={onFileItemClick} />
+        ))}
       </List>
     </div>
   );
